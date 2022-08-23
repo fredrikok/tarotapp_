56c5eb39ff26059cb69ec8994bf528dcb1f92cc5
Fredrik Olsen <83677247+darmoyasun@users.noreply.github.com>
2022-08-23 17:26:40 +0200

    Dokumentasjon

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-Sette opp react router :D
+
+Sette opp react router :D 👍
+
+
+Filter System på kort liste ❌
+
+API horoscope 
+Dokumentasjon: https://aztro.sameerkumar.website/
+    -> Lage så man kan velge hvilket tegn man vil ❌
+
+
+
+
+
+
+
+
+
--- src/pages/Horoscope.js
+++ src/pages/Horoscope.js
@@ -1,12 +1,59 @@
-import React from 'react'
+import React, { Component } from 'react';
 
-function Horoscope() {
-  return (
-    <main>
-        <h1>Daily Horoscope</h1>
-        <p>😎Choose your sign 😎</p>
-    </main>
-  )
+class Aztro extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+          json: {}
+        }
+    }
+
+    componentDidMount () {
+
+
+        const URL = 'https://aztro.sameerkumar.website?sign= <sign> &day= <day>';
+        fetch(URL, {
+            method: 'POST'
+        }).then(response => response.json())
+        .then(json => { this.setState({json}); });
+
+        console.log()
+    }
+
+    render() {
+        return (
+          <main>
+              Current Date: {this.state.json.current_date} <br />
+              Compatibility: {this.state.json.compatibility} <br />
+              Lucky Number: {this.state.json.lucky_number} <br />
+              Lucky Time: {this.state.json.lucky_time} <br />
+              Color: {this.state.json.color} <br />
+              Date Range: {this.state.json.date_range} <br />
+              Mood: {this.state.json.mood} <br />
+              Description: {this.state.json.description} <br />
+              
+          </main>
+        );
+    }
 }
 
-export default Horoscope
+export default Aztro;
+
+
+//backup
+
+// function Horoscope() {
+//   return (
+//     <main>
+//         <h1>Daily Horoscope</h1>
+//         <p>😎Choose your sign 😎</p>
+//         <button> libra </button>
+
+
+
+
+//     </main>
+//   )
+// }
+
+// export default Horoscope
