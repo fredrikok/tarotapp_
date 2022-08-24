866d4699fcc0ae76dd0c5222335cd7c63e48a587
Fredrik Olsen <83677247+darmoyasun@users.noreply.github.com>
2022-08-24 10:44:13 +0200

    credi+nav

--- src/pages/Credits.js
+++ src/pages/Credits.js
@@ -6,11 +6,14 @@ function Credits() {
         
 
     <p>API :</p>
-        <a href="https://aztro.sameerkumar.website/">Tarot Cards</a>
-        <a href="https://rws-cards-api.herokuapp.com/">Horoscope</a>
+        <a href="https://aztro.sameerkumar.website/" target="_blank">Tarot Cards</a>
+        <a href="https://rws-cards-api.herokuapp.com/" target="_blank">Horoscope</a>
 
         <p>Images :</p>
-        <a href="https://www.yoshitakaamano.com/">yoshitaka amano</a>
+        <a href="https://www.yoshitakaamano.com/" target="_blank">yoshitaka amano</a>
+        <a href="https://en.wikipedia.org/wiki/Rider%E2%80%93Waite_tarot_deck" target="_blank">Tarot Cards</a>
+
+    <p> App by Yven and Fref</p>
 
     </main>
   )
