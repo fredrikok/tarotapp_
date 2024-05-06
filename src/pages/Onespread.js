12831bcc4f18fb9654154088ac04f3181f1d0fae
Fredrik Olsen Kapoun <fredrik99k@hotmail.com>
2024-05-06 20:02:06 +0200

    updt

--- src/pages/Onespread.js
+++ src/pages/Onespread.js
@@ -1,25 +1,27 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import QuickNav from '../components/QuickNav';
-import Tarot from '../data/tarot.json';
-import questionCard from '../Img/emptycard.jpg';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import QuickNav from "../components/QuickNav";
+import Tarot from "../data/tarot.json";
+import questionCard from "../Img/emptycard.jpg";
 
 const Card = ({ img, result, meaning, desc, isReversed }) => {
   return (
-    <div className='main-box'>
+    <div className="main-box">
       <div className="cardpicked">
         <img
-          className='card'
+          className="card"
           src={img}
           alt={result}
-          style={isReversed ? { transform: 'rotateX(180deg)' } : {}}
+          style={isReversed ? { transform: "rotateX(180deg)" } : {}}
         />
       </div>
-      <div className='aside'>
-        <h3>{result} : {isReversed ? 'Reversed' : 'Upright'}</h3>
+      <div className="aside">
+        <h3>
+          {result} : {isReversed ? "Reversed" : "Upright"}
+        </h3>
         <p>{isReversed ? meaning.meaning_rev : meaning.meaning_up}</p>
         <p>{desc}</p>
-        <div className='read-more'>
+        <div className="read-more">
           <Link to={`/cardlist/${meaning.name_short}`}>Read More</Link>
         </div>
       </div>
@@ -35,51 +37,55 @@ const TarotCard = () => {
   const shuffle = () => {
     setShakeAnimation(true);
     setTimeout(() => {
-      const randomCard = Tarot.cards[Math.floor(Math.random() * Tarot.cards.length)];
+      const randomCard =
+        Tarot.cards[Math.floor(Math.random() * Tarot.cards.length)];
       setCard(randomCard);
       setQuestionCardVisible(false);
       setShakeAnimation(false);
-      setShuffleButton("Try Again"); 
+      setShuffleButton("Try Again");
     }, 1000);
   };
 
   const tryAgain = () => {
     window.location.reload();
   };
-  
-  const [shuffleButton, setShuffleButton] = useState("Shuffle"); 
-  
+
+  const [shuffleButton, setShuffleButton] = useState("Shuffle");
+
   return (
     <main>
-      <QuickNav />
-      <div className="shufflebtn">
-        {questionCardVisible ? (
-          <button onClick={shuffle}>{shuffleButton}</button>
-        ) : (
-          <button onClick={tryAgain}>Try Again</button>
+      <section className="centeredcontent">
+        <QuickNav />
+        <div className="shufflebtn">
+          {questionCardVisible ? (
+            <button onClick={shuffle}>
+              <div className="question-card-wrapper">
+                {questionCardVisible && (
+                  <img
+                    className={`question-card${shakeAnimation ? " shake" : ""}`}
+                    src={questionCard}
+                    alt="Question card"
+                  />
+                )}
+              </div>
+            </button>
+          ) : (
+            <button onClick={tryAgain}>Try Again</button>
+          )}
+        </div>
+
+        {card.name && (
+          <Card
+            img={card.image}
+            result={card.name}
+            meaning={card}
+            desc={card.desc}
+            isReversed={Math.random() >= 0.5}
+          />
         )}
-      </div>
-  
-    <div className='question-card-wrapper'>
-      {questionCardVisible && (
-        <img
-          className={`question-card${shakeAnimation ? ' shake' : ''}`}
-          src={questionCard}
-          alt='Question card'
-        />
-      )}
-    </div>
-      {card.name && (
-        <Card
-          img={card.image}
-          result={card.name}
-          meaning={card}
-          desc={card.desc}
-          isReversed={Math.random() >= 0.5}
-        />
-      )}
+      </section>
     </main>
   );
-}  
+};
 
 export default TarotCard;
